625090e59315b151f5dd228c4fb806f759e4d7de
piero gerbino <piero_gerbino@hotmail.com>
2024-05-01 09:17:43 +0100

    Update lambda_function.py to invoke SageMaker endpoint

--- lambda/src/lambda_function.py
+++ lambda/src/lambda_function.py
@@ -1,5 +1,22 @@
+# def lambda_handler(event, context):
+#     return {
+#         'statusCode': 200,
+#         'body': 'Hello, World'
+#     }
+
+import boto3
+
 def lambda_handler(event, context):
-    return {
-        'statusCode': 200,
-        'body': 'Hello, World'
-    }
+
+    # Create a SageMaker runtime client with the AWS SDK
+    client = boto3.client('sagemaker-runtime', region_name='eu-west-1')
+
+    # Specify the endpoint name and content type
+    response = client.invoke_endpoint(
+        EndpointName='hello-world-endpoint',
+        ContentType='application/json',
+        Body=b'{ "key": "value"}'
+    )
+
+    print(response)
+    return (response['Body'].read().decode('utf-8'))
